262f576ff04da48bb110195d6d80e93defd9ff67
LuduLee <99390489+LuduLee@users.noreply.github.com>
2023-10-16 13:54:19 +0300

    Add files via upload

---
+++ Boldov-4-2IS-lb/Boldov-4-2IS-lb/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyProj_Boldov_4_2IS;
+
+namespace Boldov_4_2IS_lb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = MyProj_Boldov_4_2IS.ClassLibrary_Boldov_4_2IS.Divide(3, 5);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
+ 
